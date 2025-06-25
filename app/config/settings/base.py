adc637450a5f25645aa2e65a718b47f3d38e35da
Vladyslav Mishchenko <mishchenko.online@gmail.com>
2025-06-25 15:11:53 +0300

    refactor(settings): remove commented code from base and prod

--- app/config/settings/base.py
+++ app/config/settings/base.py
@@ -25,8 +25,7 @@ APPEND_SLASH = True
 SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = os.getenv("DEBUG", "False").lower() in ("true", "1", "yes")
-DEBUG = False
+DEBUG = None
 
 ALLOWED_HOSTS = []
 
@@ -75,12 +74,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-DATABASES = {
-    # "default": {
-    #     "ENGINE": "django.db.backends.sqlite3",
-    #     "NAME": BASE_DIR / "db.sqlite3",
-    # }
-}
+DATABASES = {}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
